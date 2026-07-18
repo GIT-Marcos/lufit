d50e625312415a696a669ee3b760afaf97aa62bf
marcos <m.pardo.agustin@gmail.com>
2026-07-18 19:19:43 -0300

    feat: implement full site layout, pages, and UI component library

---
+++ src/data/contacto.ts
@@ -0,0 +1,6 @@
+export const CONTACTO = {
+  WHATSAPP_URL: 'https://wa.me/549XXXXXXXXXX',
+  INSTAGRAM_URL: 'https://instagram.com/ludmilapt',
+  EMAIL_URL: 'mailto:ludmila@email.com',
+  INSTAGRAM_HANDLE: '@ludmilapt',
+};
